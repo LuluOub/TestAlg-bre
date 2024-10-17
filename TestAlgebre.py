7a680c70e3e32672128e626980da11580b516352
LuluOub <89424164+LuluOub@users.noreply.github.com>
2024-10-17 19:03:48 +0200

    Update TestAlgebre.py

--- TestAlgebre.py
+++ TestAlgebre.py
@@ -2,11 +2,15 @@ import random
 import string
 
 
-def choisir():
-    fonction = random.choice(string.ascii_lowercase)
-    lettre1 = random.choice(string.ascii_uppercase)
-    lettre2 = random.choice(string.ascii_uppercase)
-    lettre3 = random.choice(string.ascii_uppercase)
-    lettre4 = random.choice(string.ascii_uppercase)
-    print(lettre1,",",lettre2,",",lettre3,",",lettre4,"désignent quatre ensembles non vides tels que",lettre3,"inclus",lettre1,"et",lettre4,"inclus",lettre1,'.')
-    print("Soit",fonction,":",lettre1,"-->",lettre2,"une fonction")
+fonction = random.choice(string.ascii_lowercase)
+lettre1 = random.choice(string.ascii_uppercase)
+lettre2 = random.choice(string.ascii_uppercase)
+lettre3 = random.choice(string.ascii_uppercase)
+lettre4 = random.choice(string.ascii_uppercase)
+print(lettre1,",",lettre2,",",lettre3,",",lettre4,"désignent quatre ensembles non vides tels que",lettre3,"inclus",lettre1,"et",lettre4,"inclus",lettre1,'.')
+print("Soit",fonction,":",lettre1,"-->",lettre2,"une fonction")
+print("v(",lettre3,") = ?")
+print("soit ... € ...")
+print("... € v(",lettre3,") <=> ....")
+
+print("v-1(",lettre4,") = ...")
